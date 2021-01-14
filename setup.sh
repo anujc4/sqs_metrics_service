8ae1a7bcefed53c749d4f9fb89c8f30d7287b18b
anujchandra <anujc4@gmail.com>
2021-01-14 20:43:41 +0000

    Add setup commands

--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-sudo useradd metricsservice -s /sbin/nologin -M >/dev/null 2>&1
-
 # Copy metrics service systemd files to location
 sudo cp custom_metrics.service /lib/systemd/system/
 sudo cp custom_metrics.timer /lib/systemd/system/
 sudo cp custom_metrics.target /lib/systemd/system/
 
 # Add required permissions
-sudo chmod 755 /lib/systemd/system/custom_metrics.service
-sudo chmod 755 /lib/systemd/system/custom_metrics.timer
-sudo chmod 755 /lib/systemd/system/custom_metrics.target
+sudo chmod u+x /lib/systemd/system/custom_metrics.service
+sudo chmod u+x /lib/systemd/system/custom_metrics.timer
+sudo chmod u+x /lib/systemd/system/custom_metrics.target
+
+# Stop service
+sudo systemctl stop custom_metrics.timer
+sudo systemctl stop custom_metrics.service
+
+# Disable service
+sudo systemctl disable custom_metrics.timer
+sudo systemctl disable custom_metrics.service
 
 # Enable service
 sudo systemctl enable custom_metrics.timer
@@ -19,3 +25,9 @@ sudo systemctl enable custom_metrics.service
 # Start metrics service
 sudo systemctl start custom_metrics.timer
 sudo systemctl start custom_metrics.service
+
+# Test commands
+# sudo systemctl status custom_metrics.service
+# sudo systemctl status custom_metrics.timer
+# sudo journalctl -u custom_metrics.service
+# sudo journalctl -u custom_metrics.timer
